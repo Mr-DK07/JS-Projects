b6ff0c3575c5d7dcec70e03f8c763ccf1d96b476
DK <143409270+Mr-DK07@users.noreply.github.com>
2024-03-07 06:39:41 +0000

    modify some files

---
+++ CurrencyConverter/currConverter.js
@@ -0,0 +1,7 @@
+// https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/eur/jpy.json
+// https://open.er-api.com/v6/latest/USD
+
+const BASE_URL = "https://open.er-api.com/v6/latest/USD";
+
+const dropdown = document.querySelectorAll(".dropdown select");
+
