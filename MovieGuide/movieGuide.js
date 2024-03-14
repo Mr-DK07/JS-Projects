c627c1b0368f72cbc20e810ca5701d4de4f940d9
DK <143409270+Mr-DK07@users.noreply.github.com>
2024-03-14 06:07:14 +0000

    modify app.js

--- MovieGuide/movieGuide.js
+++ MovieGuide/movieGuide.js
@@ -5,7 +5,7 @@ const inputBox = document.querySelector('.inputBox')
 // Function to fetch movie details using OMDB Api
 const getMovieInfo = async (movie) => {
     try{
-    const myAPIKey = '*********';
+    const myAPIKey = '*********'; // here you have to replace the * with your apiKey.
     const url = `http://www.omdbapi.com/?&apikey=${myAPIKey}=${movie}`;
     
     let response = await fetch(url);
